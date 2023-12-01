21c03a49d0e66276db34e1cdc40494154889f600
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-12-01 16:29:14 +0700

    learn basic py

---
+++ index.py
@@ -0,0 +1,15 @@
+# greeting = 'Helo world'
+# print(greeting)
+# addition = 2+2
+# result = addition -1
+# print(result)
+
+# input
+greeting = input('Masukan nama Anda: ')
+print(greeting)
+ 
+"""
+Output:
+Masukan nama Anda: Perseus Evans
+Perseus Evans
+"""
